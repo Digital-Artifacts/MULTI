b83ad2f51cf09345de67c0a44b331f591503f2aa
AdedamolaXL <adedamolaxl@outlook.com>
2023-09-18 16:06:52 +0100

    add pages and links

---
+++ frontend/src/app/pages/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const home = () => {
+  return (
+    <main className="">
+      
+     
+     <div>home</div>
+    </main>
+   
+    
+  )
+}
+
+export default home
