255159b5072c925ca5415ee38d4fe3a59408feca
willdera <godswillezeoke@gmail.com>
2023-09-22 14:41:16 +0100

    rebase

---
+++ backend/src/frameworks/prisma/service/stream.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Prisma, Stream, StreamTargets, User } from '@prisma/client';
+
+@Injectable()
+export class StreamService {
+  constructor(private prisma: PrismaService) { }
+
+  async Stream(
+    StreamWhereUniqueInput: Prisma.StreamWhereUniqueInput,
+  ): Promise<Stream | null> {
+    return this.prisma.stream.findUnique({
+      where: StreamWhereUniqueInput,
+    });
+  }
+
+  async streamForUser(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<any | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+      include: {
+        multistreamTargets: {
+          select: {
+            targetId: true,
+          },
+        },
+      },
+    });
+  }
+
+  async streamTargetsForUser(
+    where: any,
+    select: Prisma.UserSelect,
+  ): Promise<any | null> {
+    return this.prisma.user.findFirst({
+      where,
+      select,
+    });
+  }
+
+  async createStream(data: Prisma.StreamUncheckedCreateInput): Promise<Stream> {
+    return this.prisma.stream.create({
+      data,
+    });
+  }
+
+  async createUser(data: Prisma.UserUncheckedCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async StreamTarget(
+    where: Prisma.StreamTargetsWhereUniqueInput,
+  ): Promise<StreamTargets | null> {
+    return this.prisma.streamTargets.findUnique({
+      where,
+    });
+  }
+
+  async createStreamTarget(
+    data: Prisma.StreamTargetsUncheckedCreateInput,
+  ): Promise<StreamTargets> {
+    return this.prisma.streamTargets.create({
+      data,
+    });
+  }
+}
