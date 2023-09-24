8923ebcfe2e567f671b7ac61593e6d2f60477b3c
willdera <godswillezeoke@gmail.com>
2023-09-24 08:21:48 +0100

    Fetch user's streams

--- backend/src/core/core.controller.ts
+++ backend/src/core/core.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateStreamTargetDto, LivepeerCreateStreamDto } from 'src/dto';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import {
+  CreateStreamTargetDto,
+  GetUserStreamsDto,
+  LivepeerCreateStreamDto,
+} from 'src/dto';
 import { ValidatorPipe, CreateStreamTargetPipe } from 'src/validator/pipe';
 import { CoreService } from './core.service';
-import { StreamTargets } from '@prisma/client';
+import { StreamTargets, Stream } from '@prisma/client';
 
 @Controller('core')
 export class CoreController {
   constructor(private coreService: CoreService) { }
+
+  @Get('/stream')
+  async getStreams(
+    @Query('username') getUserStreamsDto: GetUserStreamsDto,
+  ): Promise<any> {
+    return await this.coreService.getStream(getUserStreamsDto);
+  }
 
   @Post('/stream')
   async createStream(
